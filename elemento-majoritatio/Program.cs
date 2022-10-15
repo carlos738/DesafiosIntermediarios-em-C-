27b7b64a3adf70b0d9c302f34251ca34203073e1
carlos738 <carlosasl135@hotmail.com>
2022-10-15 18:10:38 -0300

    publicando o código

---
+++ elemento-majoritatio/Program.cs
@@ -0,0 +1,84 @@
+﻿
+
+
+
+//  Desafio
+// Dado um array nums de tamanho n, retorne o elemento majoritário, isto é, o elemento que aparece o maior número de vezes no seu array.
+
+// Entrada
+// A primeira linha da entrada deverá ser o número referente ao tamanho do seu array. As demais linhas nums serão os valores da sua array.
+
+// Saída
+// A saída deverá retornar o número que aparece o maior número de vezes na sua array, ou seja, o elemento majoritário.
+
+// Exemplo 1
+
+// Entrada	Saída
+// 3
+// 7
+// 5
+// 7	7
+// Exemplo 2
+
+// Entrada	Saída
+// 9
+// 2
+// 1
+// 1
+// 1
+// 2
+// 2
+// 2
+// 1
+// 2	2
+
+using System;
+
+namespace elemento_majoritatio
+{
+
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            //Console.WriteLine("Digite a quantidade de números: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] num = new int[n];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                //Console.WriteLine("Digite o número: ");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(MajorityElement(num));
+
+        }
+        public static int MajorityElement(int[] nums)
+        {
+            int major = nums[0];
+            int count = 0;
+            int auxcount = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] != major)
+                    {
+                        if (nums[i] == nums[j])
+                            count++;
+                    }
+                }
+
+                if (count > auxcount)
+                {
+                    major = nums[i];
+                    auxcount = count;
+                    count = 0;
+                }
+
+            }
+            return major;
+        }
+    }
+}
